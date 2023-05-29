042c22d62fc57555b4fdfe921e614985a1268e01
Herbert Veloso <herbert.oliveira.veloso@gmail.com>
2023-05-29 14:45:02 -0300

    fix cors

--- api/index.js
+++ api/index.js
@@ -5,10 +5,11 @@ import fetch from 'node-fetch';
 const app = express()
 const port = 4000
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 app.post('/', async (req, res) => {
+  console.log('Chegou request')
   const { url } = req.body;
   const imageUrlData = await fetch(url);
   const buffer = await imageUrlData.arrayBuffer();
